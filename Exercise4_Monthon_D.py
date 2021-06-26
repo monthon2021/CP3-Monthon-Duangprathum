d357566664c7c10fa77764fcad6cf52826396fb6
monthon2021 <86368702+monthon2021@users.noreply.github.com>
2021-06-26 14:37:03 +0700

    Add files via upload
    My homework

---
+++ Exercise4_Monthon_D.py
@@ -0,0 +1,11 @@
+#Variable
+FE= 80  #Foundation English
+GB= 73  #General Business
+ICT= 76 #Introduction of Computer Systems
+CP= 88  #Computer Programming
+print("-----Your Score-----")
+print("Foundation English = ",FE)
+print("General Business = ",GB)
+print("Introduction of Computer Systems = ",ICT)
+print("Computer Programming = ",CP)
+print("-----Thank you-----")
